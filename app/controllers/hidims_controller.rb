4ede66a8a49ef81ef7b3d26018ee26be507a1c00
Michael Nutt <michael@Link.local>
2009-07-07 02:39:04 -0400

    way too much going on in this commit

--- app/controllers/hidims_controller.rb
+++ app/controllers/hidims_controller.rb
@@ -4,8 +4,15 @@ class HidimsController < ApplicationController
   end
   
   def create
-    @hidim = Hidim.create( params[:hidim] )
-    redirect_to @hidim
+    if params[:hidim].nil? or params[:hidim][:torrent].nil? or 
+         params[:hidim][:torrent].content_type.nil? or 
+         params[:hidim][:torrent].content_type != "application/x-bittorrent"
+      flash[:notice] = "Sorry, the file you uploaded does not appear to be a torrent"
+      redirect_to "/"
+    else
+      @hidim = Hidim.create( params[:hidim] )
+      redirect_to @hidim
+    end
   end
 
   def show

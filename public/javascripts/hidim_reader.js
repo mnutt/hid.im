bb8a0377b7a388083f954ae1d8f1e84cf1c6189b
Michael Nutt <michael@Link.local>
2009-07-09 01:57:07 -0400

    Better bookmarklet

--- public/javascripts/hidim_reader.js
+++ public/javascripts/hidim_reader.js
@@ -2,7 +2,32 @@
 
 var HidimReader = {
   init: function() {
-    alert('Now click on the hidim...');
+    var helpBox = document.createElement('div');
+    helpBox.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    helpBox.style.color = "#FFFFFF";
+    helpBox.style.fontSize = "12px";
+    helpBox.style.fontFamily = '"Droid Sans", Helvetica, sans-serif;';
+    helpBox.style.padding = "8px";
+    helpBox.style.position = "fixed";
+    helpBox.style.display = "block";
+    helpBox.style.visibility = "normal";
+    helpBox.style.top = "30px";
+    helpBox.style.textAlign = "center";
+    helpBox.style.MozBorderRadius = "5px";
+    helpBox.style.WebkitBorderRadius = "5px";
+    helpBox.style.left = ((document.body.clientWidth / 2) - 250) + "px";
+    helpBox.style.width = "500px";
+
+    var titleLabel = document.createElement('div');
+    titleLabel.style.fontSize = "24px";
+    titleLabel.innerHTML = "Hidim Decoder";
+
+    helpBox.appendChild(titleLabel);
+    helpBox.innerHTML += "Click on a hidim on this page to download it.  ";
+    helpBox.innerHTML += "Refresh the page to close the Hidim Decoder.  ";
+    helpBox.innerHTML += "Tested in FireFox 3 and Safari 4.";
+
+    document.body.appendChild(helpBox);
 
     // Initialize helper scripts
     var helpers = ["sha1.js", "base64.js", "read_png.js"];
@@ -17,12 +42,49 @@ var HidimReader = {
 
     for(var i = 0; i < images.length; i++) {
       var image = images[i];
-      image.addEventListener('click', this.decodeHidim, false);
+      image.addEventListener('click', this.addInfo, false);
     }
   },
 
-  decodeHidim: function() {
+  addInfo: function() {
     var a = PngReader.readPng(this);
+    var data = "data:application/x-bittorrent;base64,"+Base64.encode(a.file.data);
+
+    var infoButton = document.createElement('div');
+    infoButton.style.backgroundColor = "#000000";
+    infoButton.style.color = "#FFFFFF";
+    infoButton.style.fontSize = "10px";
+    infoButton.style.fontFamily = '"Droid Sans", Helvetica, sans-serif;';
+    infoButton.style.padding = "4px";
+    infoButton.style.position = "absolute";
+    infoButton.style.display = "block";
+    infoButton.style.visibility = "normal";
+    infoButton.style.top = (this.offsetTop + this.height) + "px";
+    infoButton.style.left = (this.offsetLeft) + "px";
+    infoButton.style.width = (Math.max(300, this.width) - 8) + "px";
+
+    infoButton.innerHTML = " <b>" + a.fileName + "</b>";
+    infoButton.innerHTML += " <br> Torrent Length: " + parseInt(a.length / 1024) + "KB";
+    infoButton.innerHTML += " <br> SHA1 Hash: " + a.file.sha1;
+    if(a.file.sha1 == a.sha1) {
+      infoButton.innerHTML += " (PASS)";
+    } else {
+      infoBUtton.innerHTML += " (FAIL)";
+    }
+    infoButton.innerHTML += " <br>";
+
+    var downloadLink = document.createElement('a');
+    downloadLink.href = data;
+    downloadLink.style.color = "#FFFFFF";
+    downloadLink.style.backgroundColor = "#000000";
+    downloadLink.innerHTML = "download";
+    infoButton.appendChild(downloadLink);
+
+    document.body.appendChild(infoButton);
+    return false;
+  },
+
+  decodeHidim: function() {
     document.location.href = "data:application/x-bittorrent;base64,"+Base64.encode(a.file.data);
   }
 };
